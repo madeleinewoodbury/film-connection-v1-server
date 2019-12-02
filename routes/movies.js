b229381556720246abede3c8c5b7f302efebf3d9
madeleinewoodbury <randine25@gmail.com>
2019-12-02 12:54:53 -0600

    Add route to get single movie by id

--- routes/movies.js
+++ routes/movies.js
@@ -6,11 +6,28 @@ router.get('/', (req, res) => {
   res.send('Movies route');
 });
 
-router.get('/:movie', async (req, res) => {
+// @desc    Get movies by title
+// @route   GET /api/v1/movies/:title
+// @access  Public
+router.get('/:title', async (req, res) => {
   const response = await axios.get('http://www.omdbapi.com', {
     params: {
       apikey: process.env.API_KEY,
-      s: req.params.movie
+      s: req.params.title
+    }
+  });
+
+  res.send(response.data);
+});
+
+// @desc    Get single movie by id
+// @route   GET /api/v1/movies/:id
+// @access  Public
+router.get('/movie/:id', async (req, res) => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: process.env.API_KEY,
+      i: req.params.id
     }
   });
 
