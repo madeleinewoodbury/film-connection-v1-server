24e5a09adb89311a298fea029f2e22db8fbeb5a9
madeleinewoodbury <randine25@gmail.com>
2019-12-02 13:56:08 -0600

    FilmCollection model created and route to add collection and add films to collection added

---
+++ models/FilmCollection.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const FilmCollectionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    trim: true,
+    maxlength: [500, 'Description can not be more than 500 characters']
+  },
+  films: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      poster: {
+        type: String,
+        required: true
+      },
+      year: {
+        type: String,
+        required: true
+      },
+      movieId: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+  // user: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: 'User',
+  //   required: true
+  // }
+});
+
+module.exports = mongoose.model('FilmCollection', FilmCollectionSchema);
--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const FilmCollection = require('../models/FilmCollection');
 
 router.get('/', (req, res) => {
   res.send('Movies route');
@@ -32,6 +33,54 @@ router.get('/movie/:id', async (req, res) => {
   });
 
   res.send(response.data);
+});
+
+// @desc    Create a new Film Collection
+// @route   POST /api/v1/movies/collections
+// @access  Public
+router.post('/collections/', async (req, res) => {
+  try {
+    const newCollection = new FilmCollection({
+      name: req.body.name,
+      description: req.body.description
+    });
+
+    const collection = await newCollection.save();
+    res.json(collection);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @desc    Add movie to Film Collection
+// @route   POST /api/v1/movies/collections/:id/:movieId
+// @access  Public
+router.post('/collections/:id/:movieId', async (req, res) => {
+  try {
+    const response = await axios.get('http://www.omdbapi.com', {
+      params: {
+        apikey: process.env.API_KEY,
+        i: req.params.movieId
+      }
+    });
+
+    const newFilm = {
+      title: response.data.Title,
+      poster: response.data.Poster,
+      year: response.data.Year,
+      movieId: response.data.imdbID
+    };
+
+    const collection = await FilmCollection.findById(req.params.id);
+    collection.films.unshift(newFilm);
+    await collection.save();
+
+    res.json({ collection: collection.films, movie: newFilm });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
